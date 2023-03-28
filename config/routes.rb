a7f96555117660d0e928d21b752e179f6a822ea9
thisisazeez <thisisazeez0@gmail.com>
2023-03-28 13:53:49 +0100

    rails starter

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :sharks do
+    resources :posts
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "sharks#index"
+end
